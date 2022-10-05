c7476f4c69968a14fa0d98232fe79da21f02d888
이성민 <112994232+ISFPMIN@users.noreply.github.com>
2022-10-05 11:37:17 +0900

    구구단 출력
    원하는 단수를 입력받은 뒤 반복문으로 구구단 출력

--- 5주차/prob1/prob1/mian.cpp
+++ 5주차/prob1/prob1/mian.cpp
@@ -5,10 +5,10 @@ int main(void)
 {
 	int a = 0;
 	int j;
-	printf("input number 2~9:");
+	printf("input number 2~9:"); //원하는 단수 입력
 	scanf_s("%d", &a);
 
-	if (a > 1 && a < 10)
+	if (a > 1 && a < 10) //정상적인 수 입력 시
 	{
 
 		for (j = 1; j <= 9; j++)
@@ -17,7 +17,7 @@ int main(void)
 		}
 	}
 
-	else
+	else //비정상적인 수 입력 시
 	{
 		printf("Input 2~9");
 	}
